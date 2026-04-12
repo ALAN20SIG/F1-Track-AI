d03d17051643392f174ad6e449d3c19a5c032d7f
Alan Subhash <alansubhash20@gmail.com>
2026-04-12 17:25:38 +0530

    fixed cors issue

--- backend/main.py
+++ backend/main.py
@@ -32,6 +32,7 @@ app.add_middleware(
         "http://localhost:3002",
         "http://localhost:5173",  # Vite default port
         "http://127.0.0.1:5173",
+        "https://f1-track-ai.vercel.app",
         "https://f1-track-ai-op13.vercel.app"
 
     ],
